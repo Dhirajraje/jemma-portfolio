184db40f2c6753238fa049cb9d7f40c62b19ee14
Dhiraj Salunke <dhirajraje113@gmail.com>
2022-04-24 18:06:06 +0530

    added meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,37 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <div className="flex min-h-screen flex-col items-center justify-center">
         <Head>
-          <title>Sarita Karale</title>
           <link rel="icon" href="/code_snippet.png" />
+          <title>Portfolio | Sarita Karale</title>
+          <meta name="title" content="Portfolio | Sarita Karale" />
+          <meta
+            name="description"
+            content="A front end web developer experienced in ReactJs and Angular. Computer engineering graduated from SPPU, Pune."
+          />
+
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://jemma-portfolio.vercel.app/"
+          />
+          <meta property="og:title" content="Portfolio | Sarita Karale" />
+          <meta
+            property="og:description"
+            content="A front end web developer experienced in ReactJs and Angular. Computer engineering graduated from SPPU, Pune."
+          />
+          <meta property="og:image" content="/preview.png" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://jemma-portfolio.vercel.app/"
+          />
+          <meta property="twitter:title" content="Portfolio | Sarita Karale" />
+          <meta
+            property="twitter:description"
+            content="A front end web developer experienced in ReactJs and Angular. Computer engineering graduated from SPPU, Pune."
+          />
+          <meta property="twitter:image" content="/preview.png" />
         </Head>
 
         <Component {...pageProps} />
